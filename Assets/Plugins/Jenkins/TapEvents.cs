504aa1ef1f0cf0f37ea58a8acf4ea267a41a7c06
jenkinspendragon16 <jenkins.offical@gmail.com>
2020-12-16 19:37:32 +0300

    hi

---
+++ Assets/Plugins/Jenkins/TapEvents.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+public class TapEvents : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        EventManager.OnTapStart.Invoke();
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        EventManager.OnTapRelease.Invoke();
+    }
+}
