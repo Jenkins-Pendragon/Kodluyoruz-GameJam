015cd002a0f7504d7dc8db578d1b86a9dd1da978
Muhammed Üşenmez <mhusenmez@gmail.com>
2020-12-19 15:00:21 +0300

    Update ConveyorBelt

--- Assets/[Game]/Scripts/CurvedConveyorBelt.cs
+++ Assets/[Game]/Scripts/CurvedConveyorBelt.cs
@@ -5,23 +5,12 @@ using UnityEngine;
 public class CurvedConveyorBelt : MonoBehaviour
 {
     public float speed;
-    public Rigidbody rb;
-    public float direction;
+    public Rigidbody rb;    
 
-
-
-    // Update is called once per frame
     void FixedUpdate()
     {
-        /*
         Quaternion pos = rb.rotation;
-        Debug.Log(rb.rotation.eulerAngles);
-        rb.rotation = Quaternion.AngleAxis(-45, Vector3.up);
-        Debug.Log(rb.rotation.eulerAngles);  
-        rb.MoveRotation(pos);
-        */
-        Quaternion pos = rb.rotation;
-        rb.rotation *= Quaternion.Euler(0, direction * speed * Time.fixedDeltaTime, 0); 
+        rb.rotation *= Quaternion.Euler(0, speed * Time.fixedDeltaTime, 0); 
         rb.MoveRotation(pos);
     }
 }
