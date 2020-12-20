d9610e8c255954fcbade934303c1e19e01b88487
Muhammed Üşenmez <mhusenmez@gmail.com>
2020-12-20 22:21:44 +0300

    Added DragAndDrop script

--- Assets/[Game]/Scripts/ConveyorBelt.cs
+++ Assets/[Game]/Scripts/ConveyorBelt.cs
@@ -5,10 +5,10 @@ public class ConveyorBelt : MonoBehaviour
 {
     public float speed;
     public Rigidbody rb;
-    public Vector3 direction;
+    public Vector3 direction; //To forward 0,0,-1
     
     void FixedUpdate()
-    {        
+    {  
         Vector3 pos = rb.position;
         rb.position += direction * speed * Time.fixedDeltaTime;
         rb.MovePosition(pos);  
