b7a62ccb3c3e3e8cc2ab6d06d0db9bff5b3461ba
enkalados <yakup_mknc@hotmail.com>
2020-12-20 23:15:42 +0300

    cube added

---
+++ Assets/Plugins/Yakup/CubeDrag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeDrag : MonoBehaviour
+{
+    private Vector3 moffset;
+    private float mZCoord;
+    private void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        moffset = new Vector3(gameObject.transform.position.x,3, gameObject.transform.position.z) - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = mZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + moffset;
+    }
+}
