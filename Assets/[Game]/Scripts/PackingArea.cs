bb0e37b7810861bf65d8d66de5ed284e65238acd
Muhammed Üşenmez <mhusenmez@gmail.com>
2020-12-23 19:35:02 +0300

    Update PackingArea Script

--- Assets/[Game]/Scripts/PackingArea.cs
+++ Assets/[Game]/Scripts/PackingArea.cs
@@ -9,26 +9,41 @@ public class PackingArea : MonoBehaviour
     public float itemDownScale = 0.1f;
     public float tweenDelay = 0.2f;
     private List<GameObject> packedItems = new List<GameObject>();
+    private bool isColliding;
 
     private void OnTriggerEnter(Collider other)
     {
         Item item = other.GetComponent<Item>();
-        if (item != null)
+        if (item != null && !isColliding)
         {
+            isColliding = true;            
             PackItem(other.gameObject);
+            ResetItem(other.gameObject);
         }
     }
 
     private void PackItem(GameObject go) 
     {
         packedItems.Add(go);
-        go.GetComponent<Rigidbody>().isKinematic = true;
         int count = packedItems.Count;
+        if (count > itemPoints.Count)
+        {
+            packedItems.Remove(go);
+            return;
+        }        
         for (int i = 0; i < packedItems.Count; i++)
         {
             packedItems[i].transform.DOMove(itemPoints[count - 1].points[i].position, tweenDelay);
+            packedItems[i].transform.DORotate(new Vector3(0, 90f, 90f), tweenDelay);
             packedItems[i].transform.DOScale(Vector3.one * itemDownScale, tweenDelay);
         }        
+    }
+
+    private void ResetItem(GameObject go) 
+    {
+        go.GetComponent<BoxCollider>().enabled = false;
+        go.GetComponent<Rigidbody>().isKinematic = true;
+        isColliding = false;
     }
 
 }
