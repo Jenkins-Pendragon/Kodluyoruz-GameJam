d3ad0358d7b5424bc73cae4abb551dfea2fdfa7f
Muhammed Üşenmez <mhusenmez@gmail.com>
2020-12-24 00:07:52 +0300

    Update Item Class

--- Assets/[Game]/Scripts/ItemManager/Item.cs
+++ Assets/[Game]/Scripts/ItemManager/Item.cs
@@ -8,5 +8,6 @@ public class Item : MonoBehaviour
     public string itemID;
     public Sprite toyIcon;
     public GameObject itemPrefab;
+    [HideInInspector]
     public bool isPackable = true;
 }
