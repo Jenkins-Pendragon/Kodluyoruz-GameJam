96c080510ed648806727005e6c19a8f9a28e954f
Muhammed Üşenmez <mhusenmez@gmail.com>
2020-12-23 19:14:41 +0300

    Added PackingArea script

---
+++ Assets/[Game]/Scripts/PackingArea.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PackingArea : MonoBehaviour
+{   
+    public List<Points> itemPoints;
+    public float itemDownScale = 0.1f;
+    public float tweenDelay = 0.2f;
+    private List<GameObject> packedItems = new List<GameObject>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Item item = other.GetComponent<Item>();
+        if (item != null)
+        {
+            PackItem(other.gameObject);
+        }
+    }
+
+    private void PackItem(GameObject go) 
+    {
+        packedItems.Add(go);
+        go.GetComponent<Rigidbody>().isKinematic = true;
+        int count = packedItems.Count;
+        for (int i = 0; i < packedItems.Count; i++)
+        {
+            packedItems[i].transform.DOMove(itemPoints[count - 1].points[i].position, tweenDelay);
+            packedItems[i].transform.DOScale(Vector3.one * itemDownScale, tweenDelay);
+        }        
+    }
+
+}
+
+[System.Serializable]
+public class Points 
+{
+    public Transform[] points;
+}
