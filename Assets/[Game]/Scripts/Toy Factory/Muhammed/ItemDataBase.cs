235f9463e5eecabfc610fb582b67a6b5e901937f
Muhammed Üşenmez <mhusenmez@gmail.com>
2020-12-23 21:35:00 +0300

    Update ItemDatabase script

--- Assets/[Game]/Scripts/Toy Factory/Muhammed/ItemDataBase.cs
+++ Assets/[Game]/Scripts/Toy Factory/Muhammed/ItemDataBase.cs
@@ -5,14 +5,5 @@ using UnityEngine;
 public class ItemDataBase : MonoBehaviour
 {
     public List<Item> items;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
