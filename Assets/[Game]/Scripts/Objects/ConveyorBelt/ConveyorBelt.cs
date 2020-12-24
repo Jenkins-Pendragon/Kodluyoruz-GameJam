7c6484bffff82c191af4c557c49ab6b6534f088b
Muhammed Üşenmez <mhusenmez@gmail.com>
2020-12-24 17:50:02 +0300

    Added ConveyorBelt Speed option to LevelData

--- Assets/[Game]/Scripts/Objects/ConveyorBelt/ConveyorBelt.cs
+++ Assets/[Game]/Scripts/Objects/ConveyorBelt/ConveyorBelt.cs
@@ -3,14 +3,25 @@ using UnityEngine;
 
 public class ConveyorBelt : MonoBehaviour
 {
-    public float speed;
+    private float speed;
+    private float Speed
+    { 
+        get
+        {
+            if (speed == 0)
+            {
+                speed = 0.5f * LevelManager.Instance.CurrentLevel.conveyorBeltSpeed;
+            }
+            return speed;
+        }
+    }
     public Rigidbody rb;
     public Vector3 direction; //To forward 0,0,-1
     
     void FixedUpdate()
     {  
         Vector3 pos = rb.position;
-        rb.position += direction * speed * Time.fixedDeltaTime;
+        rb.position += direction * Speed * Time.fixedDeltaTime;
         rb.MovePosition(pos);  
     }
 }
--- Assets/[Game]/Scripts/Objects/ConveyorBelt/CurvedConveyorBelt.cs
+++ Assets/[Game]/Scripts/Objects/ConveyorBelt/CurvedConveyorBelt.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class CurvedConveyorBelt : MonoBehaviour
 {
-    public float speed;
+    private float speed;
+    private float Speed
+    { get
+        {
+            if (speed == 0)
+            {
+                speed = -20f * LevelManager.Instance.CurrentLevel.conveyorBeltSpeed;
+            }
+            return speed;
+        }
+    }
     public Rigidbody rb;    
 
     void FixedUpdate()
     {
         Quaternion pos = rb.rotation;
-        rb.rotation *= Quaternion.Euler(0, speed * Time.fixedDeltaTime, 0); 
+        rb.rotation *= Quaternion.Euler(0, Speed * Time.fixedDeltaTime, 0); 
         rb.MoveRotation(pos);
     }
 }
