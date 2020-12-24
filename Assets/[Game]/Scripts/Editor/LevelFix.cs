08f86452fbf2086c21f9ee32f46f1e2e81e2e438
Muhammed Üşenmez <mhusenmez@gmail.com>
2020-12-24 12:07:18 +0300

    Added Odin Editor

--- Assets/[Game]/Scripts/Editor/LevelFix.cs
+++ Assets/[Game]/Scripts/Editor/LevelFix.cs
@@ -8,7 +8,7 @@ using UnityEditor.SceneManagement;
 
 [InitializeOnLoad]
 public class LevelFix
-{
+{    
     static LevelFix()
     {
         EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
@@ -19,24 +19,23 @@ public class LevelFix
         if (state == PlayModeStateChange.ExitingEditMode)
         {
             bool initCheck = true;
-            int sceneCount = SceneManager.sceneCount;
-            Scene lastLevel = SceneManager.GetActiveScene();
+            int sceneCount = SceneManager.sceneCount;            
             string scenePath = "Assets/[Game]/Scenes/Init.unity";
+            //string scenePath = AssetDatabase.GetAssetPath(init);
+
 
             for (int i = 0; i < sceneCount; i++)
             {
                 Scene scene = SceneManager.GetSceneAt(i);
-                if (scene.name.Contains("Level"))
+                if (!scene.name.Contains("Init"))
                 {
-                    initCheck = false;
-                    lastLevel = scene;
+                    initCheck = false;                    
                     break;
                 }
             }
 
             if (!initCheck)
-            {
-                PlayerPrefs.SetString("LastLevel", lastLevel.name);
+            {                
                 SetPlayModeStartScene(scenePath);
             }
         }
---
+++ Assets/[Game]/Scripts/Manager/InitManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class InitManager : MonoBehaviour
+{    void Start()
+    {
+        //Init Game Here
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
+        Destroy(gameObject);
+    }
+}
